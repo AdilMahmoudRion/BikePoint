ffbaadfbb486f37879e10f812e0304da4bf2475b
AdilMahmoudRion <adilmahmoudrion@gmail.com>
2021-11-14 13:59:57 +0600

    dashboard

--- src/Pages/Dashboard/BookingList/BookingList.js
+++ src/Pages/Dashboard/BookingList/BookingList.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import useAuth from "../../../hooks/useAuth";
 
 const BookingList = () => {
-    return (
-        <div>
-            <h1>booking list</h1>
-        </div>
-    );
+  const { user } = useAuth();
+  const [booking, setBooking] = useState([]);
+  useEffect(() => {
+    const url = `https://safe-ocean-13118.herokuapp.com/booking?email=${user.email}`;
+    console.log(url);
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setBooking(data));
+  }, []);
+  console.log(booking);
+  return (
+    <div>
+      <h1>booking list{booking.length}</h1>
+    </div>
+  );
 };
 
 export default BookingList;
