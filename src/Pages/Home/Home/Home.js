1b3862472c067b79fca2ea42b8fb404ce3efa8fa
AdilMahmoudRion <adilmahmoudrion@gmail.com>
2021-11-15 08:36:38 +0600

    service section added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Banner from '../Banner/Banner';
 import Bikes from '../Bikes/Bikes/Bikes';
 import Product from '../Product/Product';
+import Services from '../Services/Services';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Services></Services>
             <Product></Product>
             <Bikes></Bikes>
             
--- src/Pages/Shared/Navigation/Navigation.js
+++ src/Pages/Shared/Navigation/Navigation.js
@@ -1,158 +1,69 @@
-import React from "react";
-import { Link, NavLink } from "react-router-dom";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
-import { makeStyles } from "@mui/styles";
-import { useTheme } from "@mui/material";
-import Drawer from "@mui/material/Drawer";
-import List from "@mui/material/List";
-import Divider from "@mui/material/Divider";
-import ListItem from "@mui/material/ListItem";
-import ListItemText from "@mui/material/ListItemText";
+
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import './Navigation.css';
 import useAuth from "../../../hooks/useAuth";
+import { Link } from 'react-router-dom';
 
 const Navigation = () => {
-  const { user, logout } = useAuth();
-  
-  const theme = useTheme();
-  const useStyle = makeStyles({
-    navItem: {
-      color: "#fff",
-      textDecoration: "none",
-    },
-    navLogo: {
-      [theme.breakpoints.up("sm")]: {
-        display: "none !important",
-      },
-    },
-    navContainer: {
-      [theme.breakpoints.down("sm")]: {
-        display: "none !important",
-      },
-    },
-    navHeader: {
-      [theme.breakpoints.down("sm")]: {
-        textAlign: "right !important",
-      },
-    },
-    mobileNavIteam: {
-      textDecoration: "none",
-      color: "#000",
-    },
-  });
-
-  const { navItem, navLogo, navContainer, navHeader, mobileNavIteam } =
-    useStyle();
-
-  const [state, setState] = React.useState(false);
-
-  const list = (
-    <Box sx={{ width: 250 }} role="presentation">
-      <List>
-        <ListItem button>
-          <ListItemText>
-            <Link className={mobileNavIteam} to="/home">
-              Home
-            </Link>
-          </ListItemText>
-        </ListItem>
-        <Divider />
-        <ListItem button>
-          <ListItemText>
-            <Link className={mobileNavIteam} to="/about">
-              About
-            </Link>
-          </ListItemText>
-        </ListItem>
-        <Divider />
-        <ListItem button>
-          <ListItemText>
-            <Link className={mobileNavIteam} to="/services">
-              Services
-            </Link>
-          </ListItemText>
-        </ListItem>
-        <Divider />
-      </List>
-    </Box>
-  );
-
+  const { signInWithGoogle, user, logout } = useAuth();
+  const imgUrl = user.photoURL;
+  console.log(imgUrl)
   return (
-    <>
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-              className={navLogo}
-              onClick={() => setState(true)}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography
-              className={navHeader}
-              variant="h6"
-              component="div"
-              sx={{ flexGrow: 1 }}
-            >
-              Bike Point
-            </Typography>
+    <div>
 
-            <Box className={navContainer}>
-              <Link className={navItem} to="/home">
-                <Button color="inherit">Home</Button>
-              </Link>
-              <Link className={navItem} to="/services">
-                <Button color="inherit">Services</Button>
-              </Link>
-              <Link className={navItem} to="/about">
-                <Button color="inherit">About</Button>
-              </Link>
-            </Box>
-            <Box>
-              <Typography>{user.displayName}</Typography>
-            </Box>
-            {user?.email ? (
-              <Box>
-                <NavLink
-                  style={{ textDecoration: "none", color: "white" }}
-                  to="/dashboard"
-                >
-                  <Button color="inherit">Dashboard</Button>
-                </NavLink>
-                <Button onClick={logout} color="inherit">
-                  Logout
-                </Button>
-              </Box>
-            ) : (
-              <NavLink
-                style={{ textDecoration: "none", color: "white" }}
-                to="/login"
-              >
-                <Button color="inherit">Login</Button>
-              </NavLink>
-            )}
-          </Toolbar>
-        </AppBar>
+      <div>
+        <Navbar className="navbar" sticky="top" collapseOnSelect expand="lg">
+          <Container className="">
+            <Navbar.Brand className="fs-3 fw-bolder text-white" href="/home">
+              Booking.com
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse
+              id="responsive-navbar-nav"
+              className="justify-content-end"
+            >
+              <Nav className="me-auto">
+                <Link className="nav-link text-white" to="/home">
+                  Home
+                </Link>
+                <Link className="nav-link text-white" to="/services">
+                  Services
+                </Link>
+                <Link className="nav-link text-white" to="/about">
+                  About
+                </Link>
+              </Nav>
 
-        <div>
-          <React.Fragment>
-            <Drawer open={state} onClose={() => setState(false)}>
-              {list}
-            </Drawer>
-          </React.Fragment>
-        </div>
-      </Box>
-    </>
+              <Nav>
+                <div className="userInf">
+
+                  <p className="text-white m-0">{user.displayName}</p>
+
+                </div>
+                {user?.email ? (
+                  <div className="d-flex">
+
+                    <Link to="/dashboard" className="nav-link text-white">
+                      dashboard
+                    </Link>
+                    <button className="button" onClick={logout}>
+                      Logout
+                    </button>
+                  </div>
+                ) : (
+                  <button className="button" onClick={signInWithGoogle}>
+                    Login
+                  </button>
+                )}
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </div>
+
+
+    </div>
   );
 };
 
