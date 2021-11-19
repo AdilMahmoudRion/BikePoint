f92bd9fe7ccd5ee5dbaa63e3120d07fd7614a823
AdilMahmoudRion <adilmahmoudrion@gmail.com>
2021-11-19 16:48:43 +0600

    reg

--- src/Pages/Dashboard/MakeAdmin/MakeAdmin.js
+++ src/Pages/Dashboard/MakeAdmin/MakeAdmin.js
@@ -1,21 +1,21 @@
 import React, { useState } from 'react';
 import { Button, TextField, Alert } from '@mui/material';
-// import useAuth from '../../../hooks/useAuth';
+import useAuth from '../../../hooks/useAuth';
 
 const MakeAdmin = () => {
     const [email, setEmail] = useState('');
 
     const [success, setSuccess] = useState(false);
-    // const { token } = useAuth();
+    const { token } = useAuth();
     const handleOnBlur = e => {
         setEmail(e.target.value);
     }
     const handleAdminSubmit = e => {
         const user = { email };
-        fetch('http://localhost:5000//users/admin', {
+        fetch('http://localhost:5000/users/admin', {
             method: 'PUT',
             headers: {
-                // 'authorization': `Bearer ${token}`,
+                'authorization': `Bearer ${token}`,
                 'content-type': 'application/json'
             },
             body: JSON.stringify(user)
