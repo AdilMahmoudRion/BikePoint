035b1387f58ce1ca88a0e61820141baea5cbf244
Adil_Mahmoud Rion <adilmahmoudrion@gamil.com>
2021-11-19 09:34:42 +0600

    booking section edited

--- src/Pages/Dashboard/BookingItem/BookingItem.js
+++ src/Pages/Dashboard/BookingItem/BookingItem.js
@@ -1,16 +1,27 @@
 import React, { useEffect, useState } from 'react';
-
+import './BookingItem.css'
 const BookingItem = (props) => {
 
 const {bikeName,email,price,_id}=props.bookingItem;
 
     return (
-        <div>
-            <div>
-                <h1>{bikeName}</h1>
-                <p>{email}</p>
-                <h3>{price}</h3>
+        <div className="container">
+          
+            <div className="booking-item-details">
+                <div className="details-heading">
+                <h3>Bike Name: {bikeName}</h3>
+                <p>Contact No: {email}</p>
+                <h6>Current Price: {price}</h6>
+                </div>
+                <div className="order-status">
+                    <h3 className="order-status-section">Order Status</h3>
+                    <div className="mt-3">
+                        <p>Pending</p>
+                    </div>
+                </div>
             </div>
+          
+
         </div>
     );
 };
--- src/Pages/Dashboard/BookingList/BookingList.js
+++ src/Pages/Dashboard/BookingList/BookingList.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
+import './BookingList.css'
 import useAuth from "../../../hooks/useAuth";
 import BookingItem from "../BookingItem/BookingItem";
 
 const BookingList = () => {
   const { user } = useAuth();
-  const [booking, setBooking]= useState([]);
+  const [booking, setBooking] = useState([]);
   useEffect(() => {
     const url = `http://localhost:5000/booking?email=${user.email}`;
     console.log(url);
@@ -13,22 +14,29 @@ const BookingList = () => {
       .then((data) => setBooking(data));
   }, [user.email]);
   return (
-    <div>
-      <h1>booking list{booking.length}</h1>
-      <div>
-      {booking.map((booking) => {
-        return (
+    <div className="container">
+      <div className="booking-list-section">
+        
+      <div className="booking-list-item">
+        <div>
+          {booking.map((booking) => {
+            return (
 
-          <BookingItem
-            // eslint-disable-next-line no-undef
-            Key={booking._id}
-            bookingItem={booking}
+              <BookingItem
+                // eslint-disable-next-line no-undef
+                Key={booking._id}
+                bookingItem={booking}
 
-          ></BookingItem>
-        );
-      })}
+              ></BookingItem>
+            );
+          })}
+        </div>
       </div>
-     
+      <div className="review-section">
+        <h1>review section</h1>
+      </div>
+      </div>
+
     </div>
   );
 };
